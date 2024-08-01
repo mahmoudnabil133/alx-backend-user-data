3223f8087b426e3dfdaac32567660398b090f635
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-01 16:02:02 +0300

    done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,5 +11,5 @@ def filter_datum(fields: List[str], redaction: str,
     "filter datum function"
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
-                   f'{f}={redaction}{separator}', message)
+                         f'{f}={redaction}{separator}', message)
     return message
