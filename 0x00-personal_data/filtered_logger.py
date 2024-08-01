fd25992424969132f94c331657148dc80e101510
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-01 16:31:21 +0300

    logged

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,9 +28,9 @@ class RedactingFormatter(logging.Formatter):
         """ Redacting Formatter class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-    
+
     def format(self, record: logging.LogRecord) -> str:
         "format class method"
         record.msg = filter_datum(self.fields, self.REDACTION,
-                                    record.getMessage(), self.SEPARATOR)
+                                  record.getMessage(), self.SEPARATOR)
         return super().format(record)
