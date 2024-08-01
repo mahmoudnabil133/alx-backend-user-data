ef9ca40a02996a5c09204bf0c6bdbb5b2816a288
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-01 15:59:13 +0300

    done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,11 +3,13 @@
 filter module
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     "filter datum function"
     for f in fields:
-        message = re.sub(f'{f}=.+?{separator}',
-                         f'{f}={redaction}{separator}', message)
+        m = re.sub(f'{f}=.+?{separator}',
+                   f'{f}={redaction}{separator}', message)
     return message
