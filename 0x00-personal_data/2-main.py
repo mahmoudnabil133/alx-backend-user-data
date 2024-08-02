b81c88ee6147c6ac14260025ba7fe38ee258e0f0
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-02 03:27:58 +0300

    validate

--- 0x00-personal_data/2-main.py
+++ 0x00-personal_data/2-main.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 """
-Main file
+Main -1 file
+"""
+
+# hash_password = __import__('encrypt_password').hash_password
+
+# password = "MyAmazingPassw0rd"
+# print(hash_password(password))
+
+"""
+Main -2 file
 """
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,11 @@ def hash_password(password: str) -> bytes:
     "hash function"
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    "check if password is same hashed one"
+
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
