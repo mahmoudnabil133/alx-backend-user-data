79a602a8f5e3f29da60ba12285e993705bb3b9f0
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-07 16:27:54 +0300

    pycodeStyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,11 +12,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         "check if auth is required or not"
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """authorization header"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         "current user"
         return None
