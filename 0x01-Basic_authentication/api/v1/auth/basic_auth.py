71a27b493ff23c7f623fbaaf6bbe87de72574bd9
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-08 02:35:16 +0300

    extracted_base_64_from_args.headers

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,11 @@ from typing import List, TypeVar
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        "return value afterBasic"
+        if not authorization_header or type(authorization_header) != str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
