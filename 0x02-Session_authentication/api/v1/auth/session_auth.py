8e98e922a99e9f1874cd96e672f310af010a3a3b
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-08 23:39:40 +0300

    Success_login_Sends_Cookie_Where_You_Can_Use_to_send_another_requests

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -47,9 +47,9 @@ def login():
     email = request.form.get('email')
     password = request.form.get('password')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     user = User.search({'email': email})
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
