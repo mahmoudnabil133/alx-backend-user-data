cdfa9c9a2318964489cefe20874f96b9391ee27e
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-08 04:00:00 +0300

    got_user_instance_from_user_authorization_credetionals

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -43,3 +44,19 @@ class BasicAuth(Auth):
             return None, None
         name, password = decoded_base64_authorization_header.split(':')
         return name, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        "return user object from username and pass"
+        if not user_email or type(user_email) != str:
+            return None
+        if not user_pwd or type(user_pwd) != str:
+            return None
+        user = {'email': user_email}
+        u = User.search(user)
+        if not u:
+            return None
+        u = u[0]
+        if u.is_valid_password(user_pwd):
+            return u
+        return None
