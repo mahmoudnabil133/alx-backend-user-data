c90c8bcaa696ccd26a54816eaaa6a3c6b5d426ec
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-09 02:27:49 +0300

    db_auth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,39 +1,51 @@
 #!/usr/bin/env python3
 """
-Session Auth module
+Define class SessionDButh
 """
-from api.v1.auth.session_exp_auth import SessionExpAuth
+from .session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
-from datetime import datetime, timedelta
-from os import getenv
 
 
 class SessionDBAuth(SessionExpAuth):
-    """SessionAuth class"""
+    """
+    Definition of SessionDBAuth class that persists session data
+    in a database
+    """
 
     def create_session(self, user_id=None):
-        "create session ovarlap father method"
+        """
+        Create a Session ID for a user_id
+        Args:
+           user_id (str): user id
+        """
         session_id = super().create_session(user_id)
         if not session_id:
             return None
         kw = {
-            'user_id': user_id,
-            'session_id': session_id
+            "user_id": user_id,
+            "session_id": session_id
         }
         user = UserSession(**kw)
         user.save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        "get user id from session id"
+        """
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
         user_id = UserSession.search({"session_id": session_id})
-        if not user_id:
-            return None
-        return user_id
+        if user_id:
+            return user_id
+        return None
 
     def destroy_session(self, request=None):
         """
-        Destroy Session
+        Destroy a UserSession instance based on a
+        Session ID from a request cookie
         """
         if request is None:
             return False
