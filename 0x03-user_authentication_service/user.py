bcd0d5f9b2219d94ba208c1f950a2f8936e0755b
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-15 19:35:48 +0300

    user_table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,8 +9,8 @@ Base = declarative_base()
 class User(Base):
     "users table"
     __tablename__ = 'users'
-    id = Column(Integer, Sequence('user_id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
