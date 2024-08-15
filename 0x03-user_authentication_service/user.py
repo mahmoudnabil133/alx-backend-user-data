253739fce6a060c4506e2f2d592e74b865c37c44
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-15 19:28:28 +0300

    user_table

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+"create user table in sqlalchemy"
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
