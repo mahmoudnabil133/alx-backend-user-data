fa8ffc6b10e5a78be86290d571c15fad37d740f5
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-17 04:20:04 +0300

    gen_uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,9 +14,9 @@ def _hash_password(password: str) -> bytes:
     return hashed
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     "gen uuid"
-    return uuid4()
+    return str(uuid4())
 
 class Auth:
     """Auth class to interact with the authentication database.
